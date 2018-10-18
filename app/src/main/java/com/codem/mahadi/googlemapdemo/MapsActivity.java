7ef9518515d0a6efc2102de5126b72b5dc159fc8
codermahadi <31430772+codermahadi@users.noreply.github.com>
2018-10-18 23:17:58 +0600

    Getting Information About Locations

--- app/src/main/java/com/codem/mahadi/googlemapdemo/MapsActivity.java
+++ app/src/main/java/com/codem/mahadi/googlemapdemo/MapsActivity.java
@@ -3,6 +3,8 @@ package com.codem.mahadi.googlemapdemo;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -12,6 +14,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -21,6 +24,10 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -77,6 +84,48 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
                 mMap.addMarker(new MarkerOptions().position(userLocation).title("Your Location").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
                 mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 10));
+
+                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+                try {
+                  List<Address> listAddress =  geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+
+                  if (listAddress != null && listAddress.size() > 0){
+
+                      String address = "";
+
+                      if (listAddress.get(0).getThoroughfare() != null){
+
+                          address += listAddress.get(0).getThoroughfare() + " ";
+
+                      }
+
+                      if (listAddress.get(0).getLocality() != null){
+
+                          address += listAddress.get(0).getLocality() + " ";
+
+                      }
+
+                      if (listAddress.get(0).getPostalCode() != null){
+
+                          address += listAddress.get(0).getPostalCode() + " ";
+
+                      }
+
+                      if (listAddress.get(0).getAdminArea() != null){
+
+                          address += listAddress.get(0).getAdminArea();
+
+                      }
+
+                      Toast.makeText(MapsActivity.this, address, Toast.LENGTH_LONG).show();
+
+
+
+                  }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
             }
 
